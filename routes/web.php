bc7d24219b1cb3f75591b1b0eb0d7872a620e48e
Dejan <dejan_web@outlook.com>
2025-08-15 23:26:22 +0200

    Add route and methode for contact page

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,14 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [\App\Http\Controllers\Front\IndexController::class, 'index'])->name('index_page');
-Route::get('/blog', [\App\Http\Controllers\Front\BlogController::class, 'blog'])->name('blog_page');
-Route::get('/blog-author', [\App\Http\Controllers\Front\BlogController::class, 'blogAuthor'])->name('blog_author_page');
-Route::get('/blog-category', [\App\Http\Controllers\Front\BlogController::class, 'blogCategory'])->name('blog_category_page');
-Route::get('/blog-post', [\App\Http\Controllers\Front\BlogController::class, 'blogPost'])->name('blog_post_page');
-Route::get('/blog-search', [\App\Http\Controllers\Front\BlogController::class, 'blogSearch'])->name('blog_search_page');
-Route::get('/blog-tag', [\App\Http\Controllers\Front\BlogController::class, 'blogTag'])->name('blog_tag_page');
+
+Route::get('/contact', [\App\Http\Controllers\Front\ContactController::class, 'contact'])->name('contact_page');
+
+Route::prefix('/blog')->name('blog_')->group(function () {
+    Route::get('/', [\App\Http\Controllers\Front\BlogController::class, 'blog'])->name('page');
+    Route::get('/author', [\App\Http\Controllers\Front\BlogController::class, 'blogAuthor'])->name('author_page');
+    Route::get('/category', [\App\Http\Controllers\Front\BlogController::class, 'blogCategory'])->name('category_page');
+    Route::get('/post', [\App\Http\Controllers\Front\BlogController::class, 'blogPost'])->name('post_page');
+    Route::get('/search', [\App\Http\Controllers\Front\BlogController::class, 'blogSearch'])->name('search_page');
+    Route::get('/tag', [\App\Http\Controllers\Front\BlogController::class, 'blogTag'])->name('tag_page');
+});
