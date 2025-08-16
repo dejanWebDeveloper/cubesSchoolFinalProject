5e2f8bebacf375eb17df773bddf706d6254e4dfc
Dejan <dejan_web@outlook.com>
2025-08-16 20:11:13 +0200

    Remove unuseful class from seeders

--- database/seeders/PostsSeeder.php
+++ database/seeders/PostsSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use Faker\Factory as Faker;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
