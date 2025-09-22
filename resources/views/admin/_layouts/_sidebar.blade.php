85c684ca7a91b5542af8929faf8b4372cee973b0
Dejan <dejan_web@outlook.com>
2025-09-22 21:46:17 +0200

    Update sidebar in layout

--- resources/views/admin/_layouts/_sidebar.blade.php
+++ resources/views/admin/_layouts/_sidebar.blade.php
@@ -1,6 +1,6 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="{{route('index_page')}}" class="brand-link">
+    <a href="{{route('admin_index_page')}}" class="brand-link">
         <img src="{{url('/themes/admin/dist/img/AdminLTELogo.png')}}" alt="Cubes School Logo" class="brand-image img-circle elevation-3"
              style="opacity: .8">
         <span class="brand-text font-weight-light">Cubes Blog</span>
@@ -64,7 +64,7 @@
                     <a href="#" class="nav-link">
                         <i class="nav-icon far fa-plus-square"></i>
                         <p>
-                            Posts Author
+                            Authors
                             <i class="right fas fa-angle-left"></i>
                         </p>
                     </a>
@@ -87,7 +87,7 @@
                     <a href="#" class="nav-link">
                         <i class="nav-icon far fa-plus-square"></i>
                         <p>
-                            Post Categories
+                            Categories
                             <i class="right fas fa-angle-left"></i>
                         </p>
                     </a>
@@ -110,7 +110,7 @@
                     <a href="#" class="nav-link">
                         <i class="nav-icon far fa-plus-square"></i>
                         <p>
-                            Post Tags
+                            Tags
                             <i class="right fas fa-angle-left"></i>
                         </p>
                     </a>
@@ -156,7 +156,7 @@
                     <a href="#" class="nav-link">
                         <i class="nav-icon far fa-plus-square"></i>
                         <p>
-                            Post Comments
+                            Comments on Posts
                             <i class="right fas fa-angle-left"></i>
                         </p>
                     </a>
