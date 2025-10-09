4d20c76af5d668cd2265f85bd4e69e2402f7e043
Dejan <dejan_web@outlook.com>
2025-10-09 18:35:18 +0200

    Implement repositories for displaying data on the blog pages

--- app/Http/Controllers/Front/BlogController.php
+++ app/Http/Controllers/Front/BlogController.php
@@ -5,25 +5,31 @@ namespace App\Http\Controllers\Front;
 use App\Http\Controllers\Controller;
 use App\Models\Author;
 use App\Models\Category;
-use App\Models\Post;
-use App\Models\PostComment;
 use App\Models\Tag;
+use App\Repositories\PostRepository;
 use App\Rules\ReCaptcha;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Cache;
 
 class BlogController extends Controller
 {
+    protected $posts;
+
+    public function __construct(PostRepository $posts)
+    {
+        $this->posts = $posts;
+    }
+
     public function blog()
     {
-        $page = request('page', 1);
+        $blogPosts = $this->posts->getBlogPosts();
+        /*$page = request('page', 1);
         $cacheKey = "blogPosts_page_{$page}";
         $blogPosts = Cache::remember($cacheKey, 600, function () {
             return Post::standardRequest()
                 ->withCount('comments')
                 ->paginate(12);
         });
-        Post::addCacheKeyToIndex($cacheKey);
+        Post::addCacheKeyToIndex($cacheKey);*/
 
         return view('front.blog_pages.blog_page.blog_page', compact('blogPosts'));
     }
@@ -32,7 +38,8 @@ class BlogController extends Controller
     public function blogAuthor($id, $slug)
     {
         $author = Author::where('slug', $slug)->where('id', $id)->firstOrFail();
-        $page = request('page', 1);
+        $authorPosts = $this->posts->getAuthorPosts($author->id);
+        /*$page = request('page', 1);
         $cacheKey = "authorPosts_page_{$page}";
         $authorPosts = Cache::remember($cacheKey, 400, function () use ($author) {
             return Post::with('author')
@@ -42,7 +49,7 @@ class BlogController extends Controller
                 ->orderBy('created_at', 'desc')
                 ->paginate(4);
         });
-        Post::addCacheKeyToIndex($cacheKey);
+        Post::addCacheKeyToIndex($cacheKey);*/
         return view('front.blog_pages.blog_author_page.blog_author_page', compact(
             'authorPosts',
             'author'
@@ -52,7 +59,8 @@ class BlogController extends Controller
     public function blogCategory($id, $slug)
     {
         $category = Category::where('slug', $slug)->where('id', $id)->firstOrFail();
-        $page = request('page', 1);
+        $categoryPosts = $this->posts->getCategoryPosts($category->id);
+        /*$page = request('page', 1);
         $cacheKey = "categoryPosts_page_{$page}";
         $categoryPosts = Cache::remember($cacheKey, 400, function () use ($category) {
             return Post::with('category')
@@ -62,7 +70,7 @@ class BlogController extends Controller
                 ->orderBy('created_at', 'desc')
                 ->paginate(4);
         });
-        Post::addCacheKeyToIndex($cacheKey);
+        Post::addCacheKeyToIndex($cacheKey);*/
         return view('front.blog_pages.blog_category_page.blog_category_page', compact(
             'category',
             'categoryPosts'
@@ -71,43 +79,12 @@ class BlogController extends Controller
 
     public function blogPost($id, $slug)
     {
-        $cacheKey = "singlePost_{$id}";
-        $singlePost = Cache::remember($cacheKey, 300, function () use ($id, $slug) {
-            return Post::withCount(['comments' => function ($query) {
-                $query->where('enable', 1);
-            }])->where('slug', $slug)
-                ->where('id', $id)
-                ->where('enable', 1)
-                ->firstOrFail();
-        });
-        //increment views
-        $sessionKey = 'post_' . $singlePost->id . '_viewed';
-        if (!session()->has($sessionKey)) {
-            $singlePost->increment('views');
-            session([$sessionKey => true]);
-        }
+        $singlePost = $this->posts->getSinglePost($id, $slug);
+        $this->posts->incrementPostViews($singlePost);
         $singlePostTags = $singlePost->tags()->get();
-        $cacheKeyPrev = "prevPost_{$id}";
-        $prevPost = Cache::remember($cacheKeyPrev, 300, function () use ($id, $singlePost) {
-           return Post::where('id', '<', $singlePost->id)
-                ->where('enable', 1)
-                ->orderBy('id', 'desc')
-                ->first();
-        });
-        $cacheKeyNext = "nextPost_{$id}";
-        $nextPost = Cache::remember($cacheKeyPrev, 300, function () use ($id, $singlePost) {
-           return Post::where('id', '>', $singlePost->id)
-                ->where('enable', 1)
-                ->orderBy('id', 'asc')
-                ->first();
-        });
-        $comments = PostComment::where('post_id', $singlePost->id)
-            ->where('enable', 1)
-            ->orderBy('created_at', 'asc')
-            ->get();
-        Post::addCacheKeyToIndex($cacheKey);
-        Post::addCacheKeyToIndex($cacheKeyPrev);
-        Post::addCacheKeyToIndex($cacheKeyNext);
+        $prevPost = $this->posts->getPrevPost($id, $singlePost);
+        $nextPost = $this->posts->getNextPost($id, $singlePost);
+        $comments = $this->posts->getPostComments($singlePost);
         return view('front.blog_pages.blog_post_page.blog_post_page', compact(
             'singlePost',
             'singlePostTags',
@@ -126,13 +103,13 @@ class BlogController extends Controller
             'post_id' => ['required', 'integer', 'exists:posts,id'],
             'g-recaptcha-response' => ['required', new ReCaptcha()]
         ]);
-
-        $data['enable'] = 1;
+        $newComment = $this->posts->storeComment($data);
+        /*$data['enable'] = 1;
 
         $newComment = new PostComment();
         $newComment->fill($data);
         $newComment->save();
-        Post::clearBlogCacheForPost($data['post_id']);
+        Post::clearBlogCacheForPost($data['post_id']);*/
         return response()->json([
             'message' => 'Your comment has been submitted successfully!',
             'comment' => $newComment // optional: return comment if you want to append it via JS
@@ -142,7 +119,8 @@ class BlogController extends Controller
     public function blogTag($id, $slug)
     {
         $tag = Tag::where('slug', $slug)->where('id', $id)->firstOrFail();
-        $page = request('page', 1);
+        $tagPosts = $this->posts->getTagPosts($id, $tag);
+        /*$page = request('page', 1);
         $cacheKey = "tagPosts_page_{$page}";
         $tagPosts = Cache::remember($cacheKey, 400, function () use ($tag) {
             return $tag->posts()
@@ -152,7 +130,7 @@ class BlogController extends Controller
                 ->orderBy('created_at', 'desc')
                 ->paginate(4);
         });
-        Post::addCacheKeyToIndex($cacheKey);
+        Post::addCacheKeyToIndex($cacheKey);*/
         return view('front.blog_pages.blog_tag_page.blog_tag_page', compact(
             'tag',
             'tagPosts'
@@ -162,17 +140,7 @@ class BlogController extends Controller
     public function blogSearch(Request $request)
     {
         $query = $request->input('search');
-        $page = request('page', 1);
-
-        $cacheKey = "searchResults_" . md5($query) . "_page_{$page}";
-        $results = Cache::remember($cacheKey, 300, function () use ($query) {
-            return Post::withCount('comments')
-                ->where('heading', 'like', '%' . $query . '%')
-                ->orWhere('preheading', 'like', '%' . $query . '%')
-                ->orWhere('text', 'like', '%' . $query . '%')
-                ->paginate(4);
-        });
-        Post::addCacheKeyToIndex($cacheKey);
+        $results = $this->posts->getPostsResult($query);
         return view('front.blog_pages.blog_search_page.blog_search_page', compact(
             'results',
             'query'
--- app/Repositories/PostRepository.php
+++ app/Repositories/PostRepository.php
@@ -3,10 +3,14 @@
 namespace App\Repositories;
 
 use App\Models\Post;
+use App\Models\PostComment;
 use Illuminate\Support\Facades\Cache;
 
 class PostRepository
 {
+    /**
+     * index page
+     */
     public function getImportantPosts()
     {
         $cacheKeyImportant = "importantPost";
@@ -21,6 +25,7 @@ class PostRepository
         Post::addCacheKeyToIndex($cacheKeyImportant);
         return $importantPosts;
     }
+
     public function getLatestPosts()
     {
         $cacheKeyLatest = "latestPostsSlider";
@@ -34,5 +39,148 @@ class PostRepository
         return $latestPostsSlider;
     }
 
+    /**
+     * blog pages
+     */
+    public function getBlogPosts()
+    {
+        $page = request('page', 1);
+        $cacheKey = "blogPosts_page_{$page}";
+        $blogPosts = Cache::remember($cacheKey, 600, function () {
+            return Post::standardRequest()
+                ->withCount('comments')
+                ->paginate(12);
+        });
+        Post::addCacheKeyToIndex($cacheKey);
+        return $blogPosts;
+    }
 
+    public function getAuthorPosts($authorId)
+    {
+        $page = request('page', 1);
+        $cacheKey = "authorPosts_{$authorId}_page_{$page}";
+        $authorPosts = Cache::remember($cacheKey, 400, function () use ($authorId) {
+            return Post::with('author')
+                ->withCount('comments')
+                ->where('author_id', $authorId)
+                ->where('enable', 1)
+                ->orderBy('created_at', 'desc')
+                ->paginate(4);
+        });
+        Post::addCacheKeyToIndex($cacheKey);
+        return $authorPosts;
+    }
+
+    public function getCategoryPosts($categoryId)
+    {
+        $page = request('page', 1);
+        $cacheKey = "categoryPosts_{$categoryId}_page_{$page}";
+        $categoryPosts = Cache::remember($cacheKey, 400, function () use ($categoryId) {
+            return Post::with('category')
+                ->withCount('comments')
+                ->where('category_id', $categoryId)
+                ->where('enable', 1)
+                ->orderBy('created_at', 'desc')
+                ->paginate(4);
+        });
+        Post::addCacheKeyToIndex($cacheKey);
+        return $categoryPosts;
+    }
+
+    public function getSinglePost($id, $slug)
+    {
+        $cacheKey = "singlePost_{$id}";
+        $singlePost = Cache::remember($cacheKey, 300, function () use ($id, $slug) {
+            return Post::withCount(['comments' => function ($query) {
+                $query->where('enable', 1);
+            }])->where('slug', $slug)
+                ->where('id', $id)
+                ->where('enable', 1)
+                ->firstOrFail();
+        });
+        Post::addCacheKeyToIndex($cacheKey);
+        return $singlePost;
+    }
+    public function incrementPostViews($post)
+    {
+        $sessionKey = 'post_' . $post->id . '_viewed';
+        if (!session()->has($sessionKey)) {
+            $post->increment('views');
+            session([$sessionKey => true]);
+        }
+    }
+
+    public function getPrevPost($id, $singlePost)
+    {
+        $cacheKeyPrev = "prevPost_{$id}";
+        $prevPost = Cache::remember($cacheKeyPrev, 300, function () use ($id, $singlePost) {
+            return Post::where('id', '<', $singlePost->id)
+                ->where('enable', 1)
+                ->orderBy('id', 'desc')
+                ->first();
+        });
+        Post::addCacheKeyToIndex($cacheKeyPrev);
+        return $prevPost;
+    }
+
+    public function getNextPost($id, $singlePost)
+    {
+        $cacheKeyNext = "nextPost_{$id}";
+        $nextPost = Cache::remember($cacheKeyNext, 300, function () use ($id, $singlePost) {
+            return Post::where('id', '>', $singlePost->id)
+                ->where('enable', 1)
+                ->orderBy('id', 'asc')
+                ->first();
+        });
+        Post::addCacheKeyToIndex($cacheKeyNext);
+        return $nextPost;
+    }
+
+    public function getPostComments($singlePost)
+    {
+        return PostComment::where('post_id', $singlePost->id)
+            ->where('enable', 1)
+            ->orderBy('created_at', 'asc')
+            ->get();
+    }
+    public function storeComment(array $data)
+    {
+        $data['enable'] = 1;
+
+        $comment = new PostComment();
+        $comment->fill($data);
+        $comment->save();
+        Post::clearBlogCacheForPost($data['post_id']);
+        return $comment;
+    }
+    public function getTagPosts($id, $tag)
+    {
+        $page = request('page', 1);
+        $cacheKey = "tagPosts_{$id}_page_{$page}";
+        $tagPosts = Cache::remember($cacheKey, 400, function () use ($tag) {
+            return $tag->posts()
+                ->with('category', 'author')
+                ->withCount('comments')
+                ->where('enable', 1)
+                ->orderBy('created_at', 'desc')
+                ->paginate(4);
+        });
+        Post::addCacheKeyToIndex($cacheKey);
+        return $tagPosts;
+    }
+    public function getPostsResult($query)
+    {
+        $page = request('page', 1);
+
+        $cacheKey = "searchResults_" . md5($query) . "_page_{$page}";
+        $results = Cache::remember($cacheKey, 300, function () use ($query) {
+            return Post::withCount('comments')
+                ->where('heading', 'like', '%' . $query . '%')
+                ->orWhere('preheading', 'like', '%' . $query . '%')
+                ->orWhere('text', 'like', '%' . $query . '%')
+                ->paginate(4);
+        });
+        Post::addCacheKeyToIndex($cacheKey);
+        return $results;
+    }
 }
